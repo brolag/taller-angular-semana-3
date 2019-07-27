64874a3c321e53843b6cbd566af6ad57ec458833
Alfredo Bonilla <brolag@brolag.local>
2019-07-26 19:51:50 -0600

    Add styles for todo-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo', component: TaskListComponent },
+  { path: '', redirectTo: 'todo', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
